3d06ba69f4d87096b8ae5ca957391ee8a8f062cd
NT <t.nam11121998@gmail.com>
2024-03-10 13:01:58 +0700

    Reservations CRUD

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -4,6 +4,7 @@ import { Logger, NotFoundException } from '@nestjs/common';
 
 export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   protected abstract readonly logger: Logger;
+
   constructor(protected readonly model: Model<TDocument>) {}
 
   async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
